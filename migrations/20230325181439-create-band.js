ba8d23f2025df500fca71e38c973ac055de840fb
James Aules <james_aules@icloud.com>
2023-03-25 15:07:25 -0400

    migration

--- migrations/20230325181439-create-band.js
+++ migrations/20230325181439-create-band.js
@@ -2,14 +2,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Bands', {
-      id: {
+    await queryInterface.createTable('bands', {
+      band_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      band_id: {
         type: Sequelize.INTEGER
       },
       name: {
@@ -23,18 +20,10 @@ module.exports = {
       },
       end_time: {
         type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bands');
+    await queryInterface.dropTable('bands');
   }
 };
